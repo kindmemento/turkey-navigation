7c6ac54a253e6703806169791bb6a440ce36d203
Metin Dökmen <metindokmen@Metins-MacBook-Pro.local>
2024-04-09 13:53:21 +0300

    declare image, get its width and height, set canvas size, x and y scale, then insert image as picture

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Image mapImage = new ImageIcon("src/map/map.png").getImage();
+        int width = mapImage.getWidth(null);
+        int height = mapImage.getHeight(null);
+
+        StdDraw.setCanvasSize(width / 2, height / 2);
+        StdDraw.setXscale(0, width);
+        StdDraw.setYscale(0, height);
+
+        StdDraw.picture(width / 2, height / 2, "src/map/map.png", width, height);
+
+        StdDraw.show();
     }
 }
